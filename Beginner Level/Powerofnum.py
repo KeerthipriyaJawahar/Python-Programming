8e0dec70257137bc31e4552b7f0a163b26e30ee4
KeerthipriyaJawahar <36623657+KeerthipriyaJawahar@users.noreply.github.com>
2018-02-26 18:07:30 +0530

    Update Powerofnum.py

--- Beginner Level/Powerofnum.py
+++ Beginner Level/Powerofnum.py
@@ -1,6 +1,9 @@
-a = raw_input("Enter the Number: ")
+ a = raw_input("Enter the Number: ")
 b=raw_input("Enter the Power: ")
 n=1
-for i in range(int(b)):
-    n=n*int(a)
-print n
+if(a != "" and b != "" and a.isdigit() and b.isdigit()):
+    for i in range(int(b)):
+        n=n*int(a)
+    print n
+else:
+    print("Invalid Input")
