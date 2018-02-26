0ec55e4f089d151a9ba33a15b6d1559a0f7fd385
KeerthipriyaJawahar <36623657+KeerthipriyaJawahar@users.noreply.github.com>
2018-02-26 17:24:31 +0530

    Create NK.py

---
+++ Beginner Level/NK.py
@@ -0,0 +1,10 @@
+n = []
+sum=0
+a =  input("Enter the Value of N: ")
+k = input("Enter the value of K:")
+for i in range(1,a+1):
+    n.append(i)
+print n
+for i in range(1,k+1):
+    sum+=i
+print sum
